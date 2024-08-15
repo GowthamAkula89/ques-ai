739dbd05488748c823c36866c3d05d1f86b48ad7
GowthamAkula89 <akulagowthamroyal@gmail.com>
2024-08-15 21:09:42 +0530

    Add User Route

---
+++ backend/src/routes/user.route.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const userController = require('../controllers/user.controller')
+
+const route = express.Router();
+route.post("/register", userController.register);
+route.post("/login", userController.login);
+
+module.exports = route;
