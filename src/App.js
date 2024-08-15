4a4cdfcbb7402b5dd9139883b4f44cfc023c34b3
GowthamAkula89 <akulagowthamroyal@gmail.com>
2024-08-16 00:23:22 +0530

    Update Initial CSS

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Header from './Components/Header/header';
+import { Provider } from 'react-redux';
+import store from './Components/Redux/store';
+import HeroSection from './Components/HeroSection/heroSection';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Provider store={store}>
+        <Header/>
+        <HeroSection/>
+      </Provider>
     </div>
   );
 }
