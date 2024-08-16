a0327aa9cef68f5024871f56404e421aaa6eb5ae
GowthamAkula89 <akulagowthamroyal@gmail.com>
2024-08-16 14:22:52 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import './App.css';
-import Header from './Components/Header/header';
 import { Provider } from 'react-redux';
 import store from './Components/Redux/store';
-import HeroSection from './Components/HeroSection/heroSection';
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import WelcomePage from './Pages/welcomePage';
+import LoginOrRegisterPage from './Pages/loginOrRegisterPage';
+import { SnackbarProvider } from 'notistack';
+import HomePage from './Pages/homePage';
 function App() {
   return (
     <div className="App">
-      <Provider store={store}>
-        <Header/>
-        <HeroSection/>
-      </Provider>
+      <SnackbarProvider maxSnack={2} autoHideDuration={2000}>
+        <Provider store={store}>
+          <Router>
+            <Routes>
+              <Route path='/' element={<WelcomePage/>}/>
+              <Route path='/login' element = {<LoginOrRegisterPage/>}/>
+              <Route path='/register' element = {<LoginOrRegisterPage/>}/>
+              <Route path='/homepage' element = {<HomePage/>}/>
+            </Routes>
+          </Router>
+        </Provider>
+      </SnackbarProvider>
     </div>
   );
 }
