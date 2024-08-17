40aa7a50d651bc90184b1b8fedff3a63413572c1
GowthamAkula89 <akulagowthamroyal@gmail.com>
2024-08-17 10:54:25 +0530

    Add Auth Middleware

---
+++ backend/src/middlewares/auth.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config()
+
+const authenticateJWT = async(req, res, next) => {
+    console.log("auth")
+    let token = req.header('Authorization');
+    
+    if(!token) return res.status(401).json({ message: 'Access Denied' });
+    token = token.split(' ')[1];
+    console.log("token", token) 
+    try{
+        const verified = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = verified;
+        console.log("verified", req.user)
+        next()
+    }catch(err){
+        res.status(400).json({ message: 'Invalid Token' });
+    }
+    
+}
+
+  
+  module.exports = { authenticateJWT };
