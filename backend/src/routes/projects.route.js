69c58985c605e3dbed6a14d8a1acd0a01fb89901
GowthamAkula89 <akulagowthamroyal@gmail.com>
2024-08-17 10:59:49 +0530

    Implemented Authentication for Project routes

--- backend/src/routes/projects.route.js
+++ backend/src/routes/projects.route.js
@@ -4,7 +4,7 @@ const { authenticateJWT } = require("../middlewares/auth");
 const router = express.Router();
 router.post("/", authenticateJWT, projectsRoute.addProject);
 router.get("/", authenticateJWT, projectsRoute.getProjects);
-router.patch("/:projectId/files", projectsRoute.updateProjectFiles);
-router.patch("/:projectId/files/:fileId", projectsRoute.updateProjectFile)
-router.delete("/:projectId/files/:fileId", projectsRoute.deleteFile);
+router.patch("/:projectId/files", authenticateJWT, projectsRoute.updateProjectFiles);
+router.patch("/:projectId/files/:fileId", authenticateJWT, projectsRoute.updateProjectFile)
+router.delete("/:projectId/files/:fileId", authenticateJWT, projectsRoute.deleteFile);
 module.exports = router;
